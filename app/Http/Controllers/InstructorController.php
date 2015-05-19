8b36fa8bfecf6f9d511a13074ee4f5747092cd70
antonio pacheco <antonio_pacheco@outlook.com>
2015-05-19 15:43:06 -0700

    post instructor

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -1,12 +1,13 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Instructor;
 use App\User;
 use Chrisbjr\ApiGuard\Http\Controllers\ApiGuardController;
 use Swagger\Annotations as SWG;
-
+use Input;
+use Illuminate\Http\Request;
+use Validator;
 /**
  * @SWG\Resource(
  *     apiVersion="1.0",
@@ -17,6 +18,12 @@ use Swagger\Annotations as SWG;
  */
 class InstructorController  extends ApiGuardController {
 
+public function __construct() {
+    parent::__construct();
+}
+
+protected $rules = ['nombre' => 'required|min:5',
+						'RFC' => 'required|min:10'];
 /**
  *
  * @SWG\Api(
@@ -43,10 +50,120 @@ class InstructorController  extends ApiGuardController {
 			],200);
 	}
 
+/**
+ *
+ * @SWG\Api(
+ *   path="/instructores",
+ *   description="Referente a los Instructores",
+ *   @SWG\Operation(
+*	  method="POST", 
+*		summary="Graba un Instructor", 
+*		notes="Regresa el ID",
+*		type="Instructor", 
+*		nickname="storeInstructor",
+*		@SWG\Parameter(
+*		name="nombre", 
+*		description="nombre completo del instructor", 
+*		paramType="form", 
+*		required=true, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="direccion", 
+*		description="Direccion completa del instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="email", 
+*		description="Correo Electrónico  del instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="email2", 
+*		description="Correo Electrónico Alternativo del instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="celular", 
+*		description="Numero telefonico Móvil del Instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="telefono", 
+*		description="Numero telefonico Fijo del Instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="RFC", 
+*		description="RFC del Instructor", 
+*		paramType="form", 
+*		required=true, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="CURP", 
+*		description="CURP del Instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="cursos", 
+*		description="cursos que da el Instructor", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="activo", 
+*		description="Indica si el Instructor se encuentra disponible laboralmente", 
+*		paramType="form", 
+*		required=false, 
+*		allowMultiple=false, 
+*		type="string"
+*  		),
+*		@SWG\Parameter(
+*		name="id_cede", 
+*		description="Cede del instructor", 
+*		paramType="form", 
+*		required=true, 
+*		allowMultiple=false, 
+*		type="integer"
+*  		)
+* 	)
+* )
+*/
 
-	public function store()
+	public function store(Request $request)
 	{
-		//
+
+		$validator = Validator::make(Input::all(),$this->rules);
+		$input = Input::all();
+		$instructor = Instructor::create($input);
+		return response()->json([
+			'input' => $input,
+			'validator' => $validator,
+			'id_insstructor' => $instructor->id_instructor
+		],200);
 	}
 /**
  *
